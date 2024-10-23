de371d206ed07df69a7a55d8b129d97d8b719803
Denis Svetlakov <dps150482@gmail.com>
2024-10-23 11:01:07 +0800

    MacOS deleted

--- MarkdownKit/Sources/Common/Elements/Strikethrough/MarkdownStrikethrough.swift
+++ MarkdownKit/Sources/Common/Elements/Strikethrough/MarkdownStrikethrough.swift
@@ -8,29 +8,31 @@
 import Foundation
 
 open class MarkdownStrikethrough: MarkdownCommonElement {
-  fileprivate static let regex = "(.?|^)(\\~\\~)(?=\\S)(.+?)(?<=\\S)(\\2)"
-  
-  open var font: MarkdownFont?
-  open var color: MarkdownColor?
-  
-  open var regex: String {
-    return MarkdownStrikethrough.regex
-  }
-  
-  public init(font: MarkdownFont? = nil, color: MarkdownColor? = nil) {
-    self.font = font
-    self.color = color
-  }
-
-  public func match(_ match: NSTextCheckingResult, attributedString: NSMutableAttributedString) {
-    attributedString.deleteCharacters(in: match.range(at: 4))
-
-    attributedString.addAttribute(
-      NSAttributedString.Key.strikethroughStyle,
-      value: NSNumber.init(value: NSUnderlineStyle.single.rawValue),
-      range: match.range(at: 3)
-    )
-
-    attributedString.deleteCharacters(in: match.range(at: 2))
-  }
+    fileprivate static let regex = "(.?|^)(\\~\\~)(?=\\S)(.+?)(?<=\\S)(\\2)"
+    
+    open var font: MarkdownFont?
+    open var color: MarkdownColor?
+    open var lineSpacing: CGFloat?
+    
+    open var regex: String {
+        return MarkdownStrikethrough.regex
+    }
+    
+    public init(font: MarkdownFont? = nil, color: MarkdownColor? = nil, lineSpacing: CGFloat? = nil) {
+        self.font = font
+        self.color = color
+        self.lineSpacing = lineSpacing
+    }
+    
+    public func match(_ match: NSTextCheckingResult, attributedString: NSMutableAttributedString) {
+        attributedString.deleteCharacters(in: match.range(at: 4))
+        
+        attributedString.addAttribute(
+            NSAttributedString.Key.strikethroughStyle,
+            value: NSNumber.init(value: NSUnderlineStyle.single.rawValue),
+            range: match.range(at: 3)
+        )
+        
+        attributedString.deleteCharacters(in: match.range(at: 2))
+    }
 }
