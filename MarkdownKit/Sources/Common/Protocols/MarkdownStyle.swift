14f6380722ba4b4ca798c41ead29c9105d75425e
Denis Svetlakov <dps150482@gmail.com>
2024-10-23 10:34:35 +0800

    Added linespacing

--- MarkdownKit/Sources/Common/Protocols/MarkdownStyle.swift
+++ MarkdownKit/Sources/Common/Protocols/MarkdownStyle.swift
@@ -9,20 +9,26 @@ import Foundation
 
 /// Styling protocol for all MarkdownElements
 public protocol MarkdownStyle {
-  var font: MarkdownFont? { get }
-  var color: MarkdownColor? { get }
-  var attributes: [NSAttributedString.Key: AnyObject] { get }
+    var font: MarkdownFont? { get }
+    var color: MarkdownColor? { get }
+    var lineSpacing: CGFloat? { get }
+    var attributes: [NSAttributedString.Key: AnyObject] { get }
 }
 
 public extension MarkdownStyle {
     var attributes: [NSAttributedString.Key: AnyObject] {
         var attributes = [NSAttributedString.Key: AnyObject]()
-    if let font = font {
-        attributes[NSAttributedString.Key.font] = font
+        if let font = font {
+            attributes[NSAttributedString.Key.font] = font
+        }
+        if let color = color {
+            attributes[NSAttributedString.Key.foregroundColor] = color
+        }
+        if let lineSpacing {
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = lineSpacing
+            attributes[NSAttributedString.Key.paragraphStyle] = paragraphStyle
+        }
+        return attributes
     }
-    if let color = color {
-        attributes[NSAttributedString.Key.foregroundColor] = color
-    }
-    return attributes
-  }
 }
